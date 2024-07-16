8ff6feac3590986372875b23382b4792cc64f7ab
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2024-07-16 12:08:15 -0500

    arreglando cors

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { celebrate, Joi } = require('celebrate');
+const { jwtMiddleware } = require('../middlewares/auth');
 
 const router = express.Router();
 const cardController = require('../controllers/card');
+router.use(jwtMiddleware)
 
 router.get('/', cardController.getAllCards);
-router.post('/', cardController.createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom(validateURL),
+  }),
+}), cardController.createCard);
+
 router.delete('/:cardId', cardController.deleteCardById);
 router.put('/:cardId/likes', cardController.likeCard);
 router.delete('/:cardId/likes', cardController.dislikeCard);
